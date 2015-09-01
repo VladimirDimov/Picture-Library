550acbf01d6de28f88cb73276ab6fc2281aa4a09
VladimirDimov <vdimov1124@gmail.com>
2015-09-01 08:57:23 +0300

    login module refactored

--- script/login.js
+++ script/login.js
@@ -4,54 +4,58 @@ var userHomePage = require('script/user-home-page');
 var userUpdate = require('script/update-user');
 var q = require('node_modules/q/q.js');
 
-var login = function () {
+var login = function() {
 	$('#add-new-album').hide();
-	
+
 	$.ajax('../html/login.html', {
-		type: 'GET',
-		timeout: 5000,
-		contentType: 'text/html',
-		success: function (response) {
-			$('#wrapper')
-				.empty()
-				.append(response);
-			$('#login-container')
-				.hide()
-				.fadeIn(constants.FADEIN_TIME);
-		},
-		error: function (error) {
-			console.log(error.message);
-		}
-	})
+			type: 'GET',
+			timeout: 5000,
+			contentType: 'text/html',
+			success: function(response) {
+				$('#wrapper')
+					.empty()
+					.append(response);
+				$('#login-container')
+					.hide()
+					.fadeIn(constants.FADEIN_TIME);
+			},
+			error: function(error) {
+				console.log(error.message);
+			}
+		})
 		.then(appendEventsToButtons);
 
 	function appendEventsToButtons() {
-		$('#button-back').click(function () {
+		$('#button-back').click(function() {
 			$('#login-container')
 				.fadeOut(constants.FADEOUT_TIME, startPage.loadStartPage);
 		});
 
-		$('#button-login').click(function () {
+		$('#button-login').click(function() {
 			var name = $('#username').val();
 			var password = $('#password').val();
 
-			var user = new Parse.User();
+			logUser(name, password);
+		});
+	}
 
-			var logger = Parse.User.logIn(name, password, {
-				success: function (user) {
+	function logUser(username, password) {
+		var user = new Parse.User();
+
+		Parse.User.logIn(username, password, {
+				success: function(user) {
 					console.log('Successful logIn');
 					$('#login-container').fadeOut(constants.FADEOUT_TIME, userHomePage.loader('#wrapper'));
 
 				},
-				error: function (user, error) {
+				error: function(user, error) {
 					var $errorContainer = $('<p>');
 
 					$('#password-input-error').show();
 					$('#password-input-error').fadeOut(7000);
 				}
 			})
-				.then(userUpdate.updateCurrentUser);
-		});
+			.then(userUpdate.updateCurrentUser);
 	}
 }
 
